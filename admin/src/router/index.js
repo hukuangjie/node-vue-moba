e727807e7a400ae3dd88184317481eabeec41d69
hukuangjie <zjxshukj@gmail.com>
2020-03-01 00:57:07 +0800

    添加分类列表

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Main from '../views/Main.vue'
 import CategoryEdit from '../views/CategoryEdit.vue'
-// import CategoryList from '../views/CategoryList.vue'
+import CategoryList from '../views/CategoryList.vue'
 
 Vue.use(VueRouter)
 
@@ -11,9 +11,14 @@ const routes = [{
   name: 'main',
   component: Main,
   children: [{
-    path: '/categories/create',
-    component: CategoryEdit
-  }]
+      path: '/categories/create',
+      component: CategoryEdit
+    },
+    {
+      path: '/categories/list',
+      component: CategoryList
+    }
+  ]
 }, ]
 
 const router = new VueRouter({
