70d32f56af344e92f9fce2d835b7699b597b02b8
hukuangjie <zjxshukj@gmail.com>
2020-03-01 16:57:27 +0800

    crud 完成了.

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="create">
-    <h1>新建分类</h1>
+    <h1>{{id ? "编辑" : "新建"}}分类 {{id}}</h1>
     <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="上级分类">
+        <el-select v-model="model.parent">
+          <el-option v-for="item in parents" :key="item._id" :label="item.name" :value="item._id"></el-option>
+        </el-select>
+      </el-form-item>
       <el-form-item label="名称">
         <el-input v-model="model.name"></el-input>
       </el-form-item>
@@ -14,21 +19,42 @@
 
 <script>
 export default {
+  props: {
+    id: {}
+  },
   data() {
     return {
-      model: {}
+      model: {},
+      parents: []
     };
   },
   methods: {
     async save() {
-      const res = await this.$http.post("categories", this.model);
+      let res;
+      if (this.id) {
+        res = await this.$http.put(`categories/${this.id}`, this.model);
+      } else {
+        res = await this.$http.post("categories", this.model);
+      }
       console.log(res);
       this.$router.push("/categories/list");
       this.$message({
         type: "success",
         message: "save success"
       });
+    },
+    async fetch() {
+      const res = await this.$http.get(`categories/${this.id}`);
+      this.model = res.data;
+    },
+    async fetchParents() {
+      const res = await this.$http.get(`categories`);
+      this.parents = res.data;
     }
+  },
+  created() {
+    this.fetchParents();
+    this.id && this.fetch();
   }
 };
 </script>
--- server/router/admin/index.js
+++ server/router/admin/index.js
@@ -2,8 +2,32 @@ module.exports = app => {
   const express = require('express')
   const router = express.Router()
   const Category = require('../../models/Category')
+
+  // 新增
   router.post('/categories', async (req, res) => {
     const model = await Category.create(req.body)
+    res.send(model)
+  })
+  // 修改
+  router.put('/categories/:id', async (req, res) => {
+    const model = await Category.findByIdAndUpdate(req.params.id, req.body)
+    res.send(model)
+  })
+  // 删除
+  router.delete('/categories/:id', async (req, res) => {
+    await Category.findByIdAndDelete(req.params.id, req.body)
+    res.send({
+      succsee: true
+    })
+  })
+  // 列表
+  router.get('/categories', async (req, res) => {
+    const items = await Category.find().populate('parent').limit(10)
+    res.send(items)
+  })
+  // 查找
+  router.get('/categories/:id', async (req, res) => {
+    const model = await Category.findById(req.params.id)
     res.send(model)
   })
   app.use('/admin/api', router)
